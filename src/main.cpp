3d2a6a84174c154187f0d77d14fb5bc5b05f88c8
sascha cherevko <mr.sascha.cherevko@gmail.com>
2026-02-02 15:49:02 +0100

    update_gravity() is now called every 500 (UPDATE_SPEED_ms) milliseconds

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,24 @@
+#include <SDL3/SDL_oldnames.h>
 #include <SDL3/SDL_render.h>
+#include <SDL3/SDL_stdinc.h>
+#include <SDL3/SDL_timer.h>
 #include <SDL3/SDL_video.h>
+#include <iostream>
 #define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
 #include"grid.h"
 
+#define UPDATE_SPEED_ms 500
+
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
 
 grid game_grid = grid();
+
+Uint64 last_update = 0;
+Uint64 timer = 0;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
@@ -33,13 +42,23 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 	}
     return SDL_APP_CONTINUE;
 }
+
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
+	timer = SDL_GetTicks() - last_update;
+
 	SDL_SetRenderDrawColor(renderer, 10, 10, 10, 255);
     SDL_RenderClear(renderer);
+	
+	if(timer >= UPDATE_SPEED_ms){
+		timer = 0;
+		game_grid.update_gravity();
+		last_update = SDL_GetTicks();
+	}
+
+	std::cout << "Timer: " << timer << "\n";
 
 	game_grid.draw(renderer);
-
     SDL_RenderPresent(renderer);
 
     return SDL_APP_CONTINUE;
